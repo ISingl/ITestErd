e6845fe96be5b0ec66209093e4d3b8907af18441
Nogin Sergey <ver-s_online@mail.ru>
2018-10-17 18:39:48 +0300

    Add repositury settings files

---
+++ ErdConsole/Erd/ErdDevice.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using SnmpSharpNet;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erd
+{
+    public class Erd
+    {
+        public string IpAddress { get; private set; }
+        public string Community { get; private set; }
+
+        private string oidMonitorVoltageSignal = ".1.3.6.1.4.1.40418.2.2.3.6.0";    //INTEGER {voltageIsYes(2),voltageIsNo(1)}
+        private string oidVoltageSensorContact10 = ".1.3.6.1.4.1.40418.2.2.4.2.0";  //INTEGER
+        private string oidTestFlag = ".1.3.6.1.4.1.40418.2.2.6.1.0";                //INTEGER {on(1),off(0)}
+        private string oidTestMinVoltage = ".1.3.6.1.4.1.40418.2.2.6.2.0";          //UINTEGER32
+        private string oidTestMaxPeriod = ".1.3.6.1.4.1.40418.2.2.6.3.0";           //INTEGER
+        private string oidTestLastPeriod = ".1.3.6.1.4.1.40418.2.2.6.4.0";          //INTEGER
+        private string oidRebootContact6_8 = ".1.3.6.1.4.1.40418.2.2.2.1.0";        //INTEGER {reset(1)}
+
+        public Erd(string ipAddress, string community)
+        {
+            IpAddress = ipAddress;
+            Community = community;
+        }
+
+        public int GetMonitorVoltageSignal()
+        {
+            return Convert.ToInt32(GetDataSnrERD(oidMonitorVoltageSignal));
+        }
+        public int GetVoltageSensorContact10()
+        {
+            return Convert.ToInt32(GetDataSnrERD(oidVoltageSensorContact10));
+        }
+
+        public int GetTestFlag()
+        {
+            return Convert.ToInt32(GetDataSnrERD(oidTestFlag));
+        }
+        public void SetTestFlag(int testFlag)
+        {
+            SetDataSnrERD(oidTestFlag, testFlag);
+        }
+        public int GetTestMinVoltage()
+        {
+            return Convert.ToInt32(GetDataSnrERD(oidTestMinVoltage));
+        }
+        public void SetTestMinVoltage(int minVoltage)
+        {
+            SetDataSnrERD(oidTestMinVoltage, minVoltage);
+        }
+        public int GetTestMaxPeriod()
+        {
+            return Convert.ToInt32(GetDataSnrERD(oidTestMaxPeriod));
+        }
+        public void SetTestMaxPeriod(int maxPeriod)
+        {
+            SetDataSnrERD(oidTestMaxPeriod, maxPeriod);
+        }
+        public int GetTestLastPeriod()
+        {
+            return Convert.ToInt32(GetDataSnrERD(oidTestLastPeriod));
+        }
+
+        public void ErdReboot()
+        {
+            SetDataSnrERD(oidRebootContact6_8, 1);
+        }
+
+
+        private string GetDataSnrERD(string oid)
+        {
+            string value = "";
+            var snmp = new SimpleSnmp(IpAddress, Community);
+            Dictionary<Oid, AsnType> result = snmp.Get(SnmpVersion.Ver1, new[] { oid });
+            if (result == null)
+                return value;
+            foreach (var kvp in result)
+            {
+                value += kvp.Value;
+            }
+            return value;
+        }
+        private void SetDataSnrERD(string oid, int data)
+        {
+            var snmp = new SimpleSnmp(IpAddress, Community);
+            Pdu pdu = new Pdu
+            {
+                Type = PduType.Set
+            };
+            pdu.VbList.Add(new Oid(oidTestFlag), new Integer32(data));
+            Dictionary<Oid, AsnType> result = snmp.Set(SnmpVersion.Ver1, pdu);
+        }
+    }
+}
