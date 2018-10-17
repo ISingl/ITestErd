1e07514b193e934bd75699f9b1b9a2b1eb9d1006
Nogin Sergey <ver-s_online@mail.ru>
2018-10-17 20:35:06 +0300

    Delete usings

--- ErdConsole/Erd/ErdDevice.cs
+++ ErdConsole/Erd/ErdDevice.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using SnmpSharpNet;
+using System;
 using System.Collections.Generic;
-using SnmpSharpNet;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ErdDevice
 {
