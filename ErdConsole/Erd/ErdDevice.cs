93607b81f1956aa805d585e961882f7d53dcad63
Nogin Sergey <ver-s_online@mail.ru>
2020-09-07 01:14:00 +0400

    Revert "Delete usings"
    This reverts commit 1e07514b193e934bd75699f9b1b9a2b1eb9d1006.

--- ErdConsole/Erd/ErdDevice.cs
+++ ErdConsole/Erd/ErdDevice.cs
@@ -1,6 +1,9 @@
-﻿using SnmpSharpNet;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using SnmpSharpNet;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ErdDevice
 {
--- ErdConsole/ErdConsole/Program.cs
+++ ErdConsole/ErdConsole/Program.cs
@@ -1,5 +1,9 @@
-﻿using ErdDevice;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErdDevice;
 
 namespace ErdConsole
 {
